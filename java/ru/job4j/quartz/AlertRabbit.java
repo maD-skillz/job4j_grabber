dc4b870a6b9b61317a0482a2b77d20f5b1e3c81a
maD-skillz <warneverchang3s@gmail.com>
2021-12-17 13:09:31 +0300

    1. Quartz [#175122 #241072]

--- java/ru/job4j/quartz/AlertRabbit.java
+++ java/ru/job4j/quartz/AlertRabbit.java
@@ -13,11 +13,11 @@ import static org.quartz.SimpleScheduleBuilder.*;
 public class AlertRabbit {
 
     public static Integer load() throws IOException {
-        final Integer[] res = {0};
+        final int[] res = {0};
         try (BufferedReader rd = new BufferedReader(new FileReader("src/main/resources/rabbit.properties"))) {
             rd.lines().forEach(e -> {
                 String[] el = e.split("=");
-                if (el.length == 2) {
+                if (!el[1].isEmpty() && el.length == 2) {
                     res[0] = Integer.parseInt(el[1]);
                 } else {
                     throw  new IllegalArgumentException();
