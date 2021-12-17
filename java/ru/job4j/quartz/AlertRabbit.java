2e41af0eec48b9c81dbe467bc4a0f49f0202c7fe
maD-skillz <warneverchang3s@gmail.com>
2021-12-17 12:25:55 +0300

    1. Quartz [#175122]

---
+++ java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,54 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+
+    public static Integer load() throws IOException {
+        final Integer[] res = {0};
+        try (BufferedReader rd = new BufferedReader(new FileReader("src/main/resources/rabbit.properties"))) {
+            rd.lines().forEach(e -> {
+                String[] el = e.split("=");
+                if (el.length == 2) {
+                    res[0] = Integer.parseInt(el[1]);
+                } else {
+                    throw  new IllegalArgumentException();
+                }
+            });
+        }
+        return res[0];
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = JobBuilder.newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(load())
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
