08de09820557337d566846424fb1c649f087da22
maD-skillz <warneverchang3s@gmail.com>
2021-12-17 20:59:24 +0300

    1.1. Job c параметрами [#260360]

--- java/ru/job4j/quartz/AlertRabbit.java
+++ java/ru/job4j/quartz/AlertRabbit.java
@@ -6,6 +6,12 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
@@ -14,14 +20,33 @@ public class AlertRabbit {
 
     public static int res = 0;
 
+    private static Connection cn;
+
+    public static void init() {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("src/main/resources/rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     public static Integer load() throws IOException {
         try (BufferedReader rd = new BufferedReader(new FileReader("src/main/resources/rabbit.properties"))) {
             rd.lines().forEach(e -> {
-                String[] el = e.split("=");
-                if (!el[1].isEmpty() && el.length == 2) {
-                    res = Integer.parseInt(el[1]);
-                } else {
-                    throw  new IllegalArgumentException();
+                if (e.contains("interval")) {
+                    String[] el = e.split("=");
+                    if (!el[1].isEmpty() && el.length == 2) {
+                        res = Integer.parseInt(el[1]);
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
                 }
             });
         }
@@ -30,9 +55,15 @@ public class AlertRabbit {
 
     public static void main(String[] args) throws IOException {
         try {
+            List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = JobBuilder.newJob(Rabbit.class).build();
+            init();
+            JobDataMap data = new JobDataMap();
+            data.put("INSERT INTO rabbit(name) VALUES(white);", store);
+            JobDetail job = JobBuilder.newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(load())
                     .repeatForever();
@@ -41,12 +72,19 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (SchedulerException | InterruptedException se) {
             se.printStackTrace();
         }
     }
 
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
