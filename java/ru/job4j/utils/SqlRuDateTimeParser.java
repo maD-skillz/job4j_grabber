e9b70923806aa78952619a2c0f09a268ddd2795a
maD-skillz <warneverchang3s@gmail.com>
2021-12-23 15:22:03 +0300

    2.1. Преобразование даты [#289476 #242583]

--- java/ru/job4j/utils/SqlRuDateTimeParser.java
+++ java/ru/job4j/utils/SqlRuDateTimeParser.java
@@ -6,17 +6,6 @@ import java.util.*;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
 
-    private static String yestDay() {
-        Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT + 3"));
-        cal.add(Calendar.DATE, -1);
-        return cal.getTime().toString();
-    }
-
-    private static String today() {
-        Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT + 3"));
-        return cal.getTime().toString();
-    }
-
     private static final Map<String, String> MONTHS = Map.ofEntries(Map.entry("янв", "1"), Map.entry("фев", "2"),
             Map.entry("мар", "3"), Map.entry("апр", "4"), Map.entry("май", "5"), Map.entry("июн", "6"),
             Map.entry("июл", "7"), Map.entry("авг", "8"), Map.entry("сен", "9"), Map.entry("окт", "10"),
@@ -24,21 +13,27 @@ public class SqlRuDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) throws Exception {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy, HH:mm");
-        String yesterday;
-        String today;
+        Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT + 3"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd, HH:mm");
         String[] date;
         LocalDateTime ldt = null;
         if (parse.contains("вчера")) {
             date = parse.split(",");
-            yesterday = yestDay() + date[1];
-            ldt = LocalDateTime.parse(yesterday, formatter);
+            cal.add(Calendar.DATE, -1);
+            ldt = LocalDateTime.parse(cal.getTime() + date[1], formatter);
         } else if (parse.contains("сегодня")) {
             date = parse.split(",");
-            today = today() + date[1];
-            ldt = LocalDateTime.parse(today, formatter);
+            ldt = LocalDateTime.parse(cal.getTime() + date[1], formatter);
         } else {
-            ldt = LocalDateTime.parse(parse, formatter);
+            date = parse.split(" ");
+            String[] year = date[2].split(",");
+            if (date[0].length() == 1) {
+                ldt = LocalDateTime.parse("20" + year[0] + "-" + MONTHS.get(date[1])
+                        + "-" + "0" + date[0] + "T" + date[3], formatter);
+            } else {
+                ldt = LocalDateTime.parse("20" + year[0] + "-" + MONTHS.get(date[1])
+                        + "-" + date[0] + "T" + date[3], formatter);
+            }
         }
         return ldt;
     }
