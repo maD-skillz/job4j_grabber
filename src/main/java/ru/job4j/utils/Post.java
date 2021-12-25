87f53198886c3e2d03acb6563d58f5dd88a7a77d
maD-skillz <warneverchang3s@gmail.com>
2021-12-25 17:14:03 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/utils/Post.java
+++ src/main/java/ru/job4j/utils/Post.java
@@ -98,7 +98,8 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(created, post.created);
+        return id == post.id && Objects.equals(title, post.title)
+                && Objects.equals(link, post.link) && Objects.equals(created, post.created);
     }
 
     @Override
