3907360ff92a4debbd2885139b3dbf23b0e1c456
maD-skillz <warneverchang3s@gmail.com>
2021-12-26 18:22:45 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/html/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/html/SqlRuParse.java
@@ -2,20 +2,14 @@ package ru.job4j.grabber.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import ru.job4j.grabber.Parse;
-import ru.job4j.grabber.ParseToPost;
 import ru.job4j.grabber.Post;
 import ru.job4j.grabber.utils.DateTimeParser;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SqlRuParse implements Parse {
-
-    private Post post = new Post();
 
     private final DateTimeParser dateTimeParser;
 
@@ -23,30 +17,23 @@ public class SqlRuParse implements Parse {
         this.dateTimeParser = dateTimeParser;
     }
 
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 5; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            Element time = td.parent().child(5);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(time.text());
-        }
-        }
-    }
-
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) throws Exception {
         List<Post> list = new ArrayList<>();
-        list.add(detail(link));
+        for (int i = 1; i <= 5; i++) {
+            String sheet = "https://www.sql.ru/forum/job-offers/" + i;
+            list.add(detail(sheet));
+        }
         return list;
     }
 
     @Override
-    public Post detail(String link) throws IOException {
-        ParseToPost parse = new ParseToPost();
-        return parse.parseToPost(link);
+    public Post detail(String link) throws Exception {
+        Document doc = Jsoup.connect(link).get();
+        String dateParse = doc.select(".msgFooter").get(0).text();
+        String[] date = dateParse.split("\\[");
+        return new Post(doc.select(".messageHeader").get(0).ownText(),
+                link,
+                doc.select(".msgBody").get(1).text(), dateTimeParser.parse(date[0].trim()));
     }
 }
