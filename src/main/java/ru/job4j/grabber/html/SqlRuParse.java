6268ea9dcf09564ae34ab2b3d0513a173717f94e
maD-skillz <warneverchang3s@gmail.com>
2021-12-26 21:23:26 +0300

    2.4. SqlRuParse [#285213 #244372]

--- src/main/java/ru/job4j/grabber/html/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/html/SqlRuParse.java
@@ -2,7 +2,10 @@ package ru.job4j.grabber.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import ru.job4j.grabber.Parse;
+import ru.job4j.grabber.ParseToPost;
 import ru.job4j.grabber.Post;
 import ru.job4j.grabber.utils.DateTimeParser;
 
@@ -17,14 +20,32 @@ public class SqlRuParse implements Parse {
         this.dateTimeParser = dateTimeParser;
     }
 
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                Element time = td.parent().child(5);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(time.text());
+            }
+        }
+    }
+
     @Override
     public List<Post> list(String link) throws Exception {
-        List<Post> list = new ArrayList<>();
+        List<String> linkList = new ArrayList<>();
+        List<Post> postList = new ArrayList<>();
         for (int i = 1; i <= 5; i++) {
             String sheet = "https://www.sql.ru/forum/job-offers/" + i;
-            list.add(detail(sheet));
+            linkList.add(sheet);
         }
-        return list;
+        for (String e : linkList) {
+            postList.add(detail(e));
+        }
+        return postList;
     }
 
     @Override
