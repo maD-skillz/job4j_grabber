fe218442a8d58ac28b6063b3455e7175bc6bca0a
maD-skillz <warneverchang3s@gmail.com>
2022-01-14 13:27:05 +0300

    1. Реализация кеша на SoftReference [#1592 #250307]

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -11,11 +11,20 @@ public abstract class AbstractCache<K, V> {
     protected final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public void put(K key, V value) {
-
+        cache.put(key, new SoftReference<>(value));
     }
 
-    public V get(K key) {
-        return null;
+    public V get(K key) throws IOException {
+        SoftReference<V> softNull = null;
+        SoftReference<V> softCache = cache.getOrDefault(key, softNull);
+        V getRef = softCache.get();
+        if (getRef == null) {
+          V loadV = load(key);
+            cache.put(key, softCache);
+        } else {
+            return getRef;
+        }
+        return getRef;
     }
 
     protected abstract V load(K key) throws IOException;
--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -1,9 +1,9 @@
 package ru.job4j.cache;
 
 import java.io.IOException;
-import java.lang.ref.SoftReference;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
+
 
 
 public class DirFileCache extends AbstractCache<String, String> {
@@ -15,19 +15,8 @@ public class DirFileCache extends AbstractCache<String, String> {
     }
 
     @Override
-    public void put(String key, String value) {
-        cache.put(key, new SoftReference<>(value));
-    }
-
-    @Override
-    public String get(String key) {
-        SoftReference<String> getKey = cache.get(key);
-        return getKey != null ? getKey.get() : null;
-    }
-
-    @Override
     protected String load(String key) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(key)));
+        return Files.readString(Path.of(cachingDir, key));
     }
 
 }
