4ef594d43f16d722014d7c5c08178a74867dbf82
maD-skillz <warneverchang3s@gmail.com>
2022-01-22 18:05:34 +0300

    0. Принцип единственной ответственности [#4913]

---
+++ src/main/java/ru/job4j/SRPdisaster.java
@@ -0,0 +1,43 @@
+package ru.job4j;
+
+
+public class SRPdisaster {
+
+    String text;
+    String author;
+    int length;
+
+    String getText() {
+        return text;
+    }
+    void setText(String s) {
+
+    }
+
+    String getAuthor() {
+        return author;
+    }
+    void setAuthor(String s) {
+
+    }
+
+    int getLength() {
+        return length;
+    }
+
+    void setLength(int k) {
+
+    }
+
+    void allLettersToUpperCase() {
+
+    }
+
+    void findSubTextAndDelete(String s) {
+
+    }
+
+    void printText() {
+
+    }
+}
